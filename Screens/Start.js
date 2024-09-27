104e5ba4945aa8f3b32f2a538ada2a4083f8723f
peihao <pei.h@northeastern.edu>
2024-09-27 15:47:45 -0700

    Created a empty start component and import the start screen to App.js

---
+++ Screens/Start.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+
+export default function Start() {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  )
+}
