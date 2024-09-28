644a368c6e90e2ed91de9da32ff98254500a5397
peihao <pei.h@northeastern.edu>
2024-09-28 13:23:20 -0700

    added expo lineargradient package, and set the background of the three screens using lineargradient component.

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Start from './Screens/Start';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <LinearGradient
+        colors={["#f0fff0", "#adff2f", "#00ff7f"]}
+        style={styles.background}>
       <StatusBar style="auto" />
       <Start />
+      </LinearGradient>
     </View>
   );
 }
@@ -17,5 +22,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
